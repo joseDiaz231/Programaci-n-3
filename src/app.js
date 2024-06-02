8cf107e31afe3877a43785855aa7961c8e92d00c
joseDiaz231 <62890811+joseDiaz231@users.noreply.github.com>
2024-06-02 10:51:20 +0000

    segundo codigo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("Hola Mundo")
+const express = require('express');
+const app = express();
+const { Home, Dashboard } = require('./controller.js');
+
+const port = 4000 || process.env.PORT;
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname +'/views');
+app.use(express.static(__dirname + '/public'));
+
+
+//rutas
+app.get('/', Home);
+app.get('/dashboard', Dashboard);
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
